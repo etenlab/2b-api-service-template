1dbd22362598f3b82951691042b1804fa91c0da3
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-16 19:42:04 -0500

    removed unneeded config from template

--- src/main/kotlin/org/eten/apiservicetemplate/core/AppConfig.kt
+++ src/main/kotlin/org/eten/apiservicetemplate/core/AppConfig.kt
@@ -10,25 +10,10 @@ import java.net.InetAddress
 class AppConfig(
 
     @Value("\${server.hostname}")
-    val hostname: String,
-
-    @Value("\${server.jwtSecret}")
-    val jwt_secret: String,
-
-    @Value("\${awsAccessKeyId}")
-    val awsAccessKeyId: String,
-
-    @Value("\${awsSecretAccessKey}")
-    val awsSecretAccessKey: String,
-
-    @Value("\${email.server}")
-    val emailServer: String,
+    val hostname: String,,
 
     @Value("\${env}")
     val env: ConfigEnv,
-
-    @Value("\${s3_url_prefix}")
-    val s3_url_prefix: String,
 
     @Value("\${spring.kafka.bootstrap-servers}")
     val kafka_bootstrap_address: String,
